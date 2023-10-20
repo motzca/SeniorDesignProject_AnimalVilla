8c6276d06adbabd5479c25d50a217472292bfde6
motzca <finnickodair11@gmail.com>
2023-10-20 16:23:40 -0400

    Initial push

---
+++ Assets/GameManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject cardGameObject;
+    public CardController mainCardController;
+    public SpriteRenderer cardSpriteRenderer;
+    float fMovingSpeed = 1f;
+    Vector2 pos;
+
+    void Start()
+    {
+    }
+    void Update()
+    {
+        //Movement
+        if (Input.GetMouseButton(0) && mainCardController.isMouseOver)
+        {
+            Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            cardGameObject.transform.position = pos;
+        }
+        else
+        {
+            cardGameObject.transform.position = Vector2.MoveTowards(cardGameObject.transform.position, new Vector2(0,0), fMovingSpeed);
+        }
+
+        //Checking right side
+        if (cardGameObject.transform.position.x > 2)
+        {
+            cardSpriteRenderer.color = Color.green;
+        }
+        else if (cardGameObject.transform.position.x < -2)
+        {
+            cardSpriteRenderer.color = Color.red;
+        }
+        else
+        {
+            cardSpriteRenderer.color = Color.white;
+        }
+    }
+}
