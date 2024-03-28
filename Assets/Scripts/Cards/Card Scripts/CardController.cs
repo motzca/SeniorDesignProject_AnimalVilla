563832353f1e221d7076e2bc6ce238095ef5fd94
motzca <finnickodair11@gmail.com>
2024-03-28 19:34:12 -0400

    Adding back things from main menu

--- Assets/Scripts/Cards/Card Scripts/CardController.cs
+++ Assets/Scripts/Cards/Card Scripts/CardController.cs
@@ -1,26 +1,30 @@
 using UnityEngine;
 using System.Collections;
 
-
 public class CardController : MonoBehaviour
 {
     public Card card;
+    
     private BoxCollider2D thisCard;
     private bool isDragging;
     private Vector3 dragStartPosition; 
     private Vector3 lastPosition;
     private CardGravity cardGravity;
 
-    private void Start()
-    {
-        thisCard = GetComponent<BoxCollider2D>();
-        cardGravity = GetComponent<CardGravity>();
-    }
+    private SpriteRenderer frontSpriteRenderer; 
+
+private void Start()
+{
+    thisCard = GetComponent<BoxCollider2D>();
+    cardGravity = GetComponent<CardGravity>();
+    frontSpriteRenderer = GetComponent<SpriteRenderer>();
+}
+
+
 
     private void Update()
     {
         HandleTouchInput();
-        HandleMouseInput();
     }
 
     private void HandleTouchInput()
@@ -57,48 +61,30 @@ public class CardController : MonoBehaviour
         }
     }
 
-    private void HandleMouseInput()
-    {
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            if (thisCard.OverlapPoint(mousePos))
-            {
-                isDragging = true;
-                dragStartPosition = transform.position; 
-            }
-        }
-
-        if (Input.GetMouseButton(0) && isDragging)
-        {
-            transform.position = new Vector3(mousePos.x, mousePos.y, transform.position.z);
-            lastPosition = transform.position;
-        }
-
-        if (Input.GetMouseButtonUp(0) && isDragging)
-        {
-            isDragging = false;
-            ProcessSwipeEnd(); 
-        }
-    }
-
     private void ProcessSwipeEnd()
     {
         Vector3 swipeDirection = lastPosition - dragStartPosition;
         float horizontalDistance = Mathf.Abs(swipeDirection.x);
         float verticalDistance = Mathf.Abs(swipeDirection.y);
 
-        if (horizontalDistance > verticalDistance && horizontalDistance > 1.0f) 
+        float minSwipeDistance = 2.5f; 
+
+        if (horizontalDistance > verticalDistance && horizontalDistance > minSwipeDistance)
         {
             bool swipedRight = swipeDirection.x > 0;
             GameManager.Instance.ProcessSwipeResult(swipedRight, true);
+            StartCoroutine(FlipCardToShowFront());
         }
         else
         {
             Debug.Log("Swipe was unclear or not horizontal enough. No card change.");
-            GameManager.Instance.ProcessSwipeResult(false, false);
+            StartCoroutine(ResetCardPosition());
         }
+    }
+
+    private IEnumerator ResetCardPosition()
+    {
+        yield return AnimateCardToPosition(dragStartPosition);
     }
 
     private IEnumerator AnimateCardToPosition(Vector3 targetPosition)
@@ -106,14 +92,72 @@ public class CardController : MonoBehaviour
         float elapsedTime = 0;
         float totalTime = 0.5f;
         Vector3 startPosition = transform.position;
+        float startAlpha = frontSpriteRenderer.color.a; 
 
         while (elapsedTime < totalTime)
         {
-            transform.position = Vector3.Lerp(startPosition, targetPosition, elapsedTime / totalTime);
+            float progress = elapsedTime / totalTime;
+            transform.position = Vector3.Lerp(startPosition, targetPosition, progress);
+
+            float alpha = Mathf.Lerp(startAlpha, 1f, progress); 
+            frontSpriteRenderer.color = new Color(1f, 1f, 1f, alpha);
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = targetPosition;
+        frontSpriteRenderer.color = Color.white;
+    }
+
+    private IEnumerator LiftAndFlipCard()
+    {
+        Vector3 liftedPosition = new Vector3(transform.position.x, transform.position.y + 0.1f, transform.position.z);
+        yield return StartCoroutine(AnimateCardToPosition(liftedPosition, 0.1f));
+
+        yield return StartCoroutine(FlipCardToShowFront());
+    }
+
+    private IEnumerator AnimateCardToPosition(Vector3 targetPosition, float duration)
+    {
+        float elapsedTime = 0;
+        Vector3 startPosition = transform.position;
+
+        while (elapsedTime < duration)
+        {
+            transform.position = Vector3.Lerp(startPosition, targetPosition, elapsedTime / duration);
             elapsedTime += Time.deltaTime;
             yield return null;
         }
 
         transform.position = targetPosition;
     }
+
+    private IEnumerator FlipCardToShowFront()
+    {
+        transform.position = dragStartPosition;
+
+        float flipDuration = 0.5f;
+        Vector3 originalScale = transform.localScale; 
+
+        frontSpriteRenderer.enabled = false; 
+
+        for (float t = 0; t <= 0.5f; t += Time.deltaTime / (flipDuration / 2))
+        {
+            transform.localScale = new Vector3(Mathf.Lerp(originalScale.x, 0, t), originalScale.y, originalScale.z);
+            yield return null;
+        }
+
+        for (float t = 0; t <= 0.5f; t += Time.deltaTime / (flipDuration / 2))
+        {
+            transform.localScale = new Vector3(Mathf.Lerp(0, originalScale.x, t), originalScale.y, originalScale.z);
+            if (t > 0.25f) 
+            {
+                frontSpriteRenderer.enabled = true;
+            }
+            yield return null;
+        }
+
+        transform.localScale = originalScale;
+    }
+
 }
